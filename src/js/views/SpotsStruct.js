b4054bdf46d89bb6db72bf120ce6498286ab6659
Alice Lemerre <al.lemerre@laposte.net>
2024-06-26 00:46:14 +0200

    design des cards

--- src/js/views/SpotsStruct.js
+++ src/js/views/SpotsStruct.js
@@ -1,26 +1,132 @@
 import { getNavbarStructure } from "../components/Navbar.js";
 import { getFooterStructure } from "../components/Footer.js";
 
+  const cards = [
+    {
+        tag: "div",
+        props: { class: "card" },
+    },
+    {
+        tag: "div",
+        props: { class: "card" },
+    },
+    {
+        tag: "div",
+        props: { class: "card" },
+    },
+    {
+        tag: "div",
+        props: { class: "card" },
+    },
+    {
+        tag: "div",
+        props: { class: "card" },
+    },
+    {
+        tag: "div",
+        props: { class: "card" },
+    },
+    {
+        tag: "div",
+        props: { class: "card" },
+    },
+    {
+        tag: "div",
+        props: { class: "card" },
+    }
+];
+
 export default function SpotsStruct() {
+   
     return {
         tag: "div",
         children: [
             getNavbarStructure(),
             {
                 tag: "h1",
-                children: ["Nos Spots"]
+                children: ["Découvrez les meilleurs spots"]
             },
             {
-                tag: "div",
-                props: { class: "page-size" },
+                tag: "section",
+                props: { class: "section1" },
                 children: [
                     {
                         tag: "p",
-                        children: ["Découvrez tous nos spots."]
+                        props: { class: "header-desc" },
+                        children: ["Nous avons recensé tous les meilleurs endroits d’où observer les Jeux Olympiques. Chaque spot est étiqueté par épreuve, lieu et capacité d’accueil"]
                     }
+                ]
+            },
+            {
+                tag: "section",
+                props: { class: "section-selection" },
+                children: [
+                    {
+                        tag: "h2",
+                        children: ["Notre sélection"]
+                    },
+                    {
+                        tag: "p",
+                        children: ["Les plus beaux spots de Paris"]
+                    },
+                    {
+                        tag: "div",
+                        props: { class: "slider-container" },
+                        children: [
+                            {
+                                tag: "div",
+                                props: { class: "slider" },
+                                children: cards.slice(0, 6).map(card => ({
+                                    tag: "div",
+                                    props: { class: "card" },
+                                    children: card.children
+                                }))
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                tag: "section",
+                props: { class: "section-selection" },
+                children: [
+                    {
+                        tag: "h2",
+                        children: ["Trouvez l'endroit idéal"]
+                    },
+                    {
+                        tag: "p",
+                        children: ["Dénichez le spot qu’il vous faut avec notre filtre de recherche"]
+                    },
+                    {
+                        tag: "div",
+                        props: { class: "spots-list-container" },
+                        children: [
+                            {
+                            tag: "div",
+                            props: { class: "spots-list" },
+                            children: cards.map(card => ({
+                                tag: "div",
+                                props: { class: "card" },
+                                
+                                children:  [
+                                    tag: "div",
+                                    props: { class: "tag-list" },
+                                    children: spots.map(spot => ({
+                                        tag: "div",
+                                        props: { class: "card-tag" },
+                                    }))
+                                ]
+                            }))
+                        }
+                        ]
+                    }
+                
                 ]
             },
             getFooterStructure()
         ]
     };
 }
+
+
