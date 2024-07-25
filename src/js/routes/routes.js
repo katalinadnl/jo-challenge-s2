12c6d041d8728517a09c56b18616f37591a1bc5d
Catalina DANILA <catalinadanila6@gmail.com>
2024-07-25 10:19:06 +0200

    clean code

---
+++ src/js/routes/routes.js
@@ -0,0 +1,17 @@
+import HomeStruct from "../views/HomeStruct.js";
+import SportsStruct from "../views/SportsStruct.js";
+import EventsStruct from "../views/EventsStruct.js";
+import MapStruct from "../views/MapStruct.js";
+import SpotsStruct from "../views/SpotsStruct.js";
+import InformationsStruct from "../views/InformationsStruct.js";
+import Page404Struct from "../views/Page404Struct.js";
+
+export const routes = [
+    { path: "/", component: HomeStruct },
+    { path: "/sports", component: SportsStruct },
+    { path: "/evenements", component: EventsStruct },
+    { path: "/carte", component: MapStruct },
+    { path: "/spots", component: SpotsStruct },
+    { path: "/spots/:id", component: InformationsStruct },
+    { path: "*", component: Page404Struct }
+];
